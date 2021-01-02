1220a0bb1b2ba9d459688d39371bd8a9d642ddce
Usamasandhu <71733850+Usamasandhu@users.noreply.github.com>
2021-01-02 19:30:12 +0500

    li

--- src/services/Generic.js
+++ src/services/Generic.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL="http://localhost:4000/api/";
+axios.defaults.baseURL="https://dashboard.heroku.com/apps/sp18-bse-088-nodelab/api/";
 axios.defaults.headers.common["x-auth-token"]=localStorage.getItem("token");
 class Generic{
     constructor(){}
